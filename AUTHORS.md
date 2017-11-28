0ad6bb868ffb9c31cf9f14ab5d59d38957837de1
quixote15 <santos2tiago@gmail.com>
2017-11-28 13:21:07 -0300

    Download of profile data

---
+++ AUTHORS.md
@@ -0,0 +1,7 @@
+# TIBO
+
+* [Hélio Kröger](https://github.com/heliojuniorkroger)
+* [Neto Jocelino](https://github.com/netojocelino)
+* [Tiago Santos](https://github.com/quixote15)
+
+
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
 const colors = require('colors')
+var fs = require('fs');
+const download = require('image-downloader')
 
 let tiboData = {}
 
@@ -7,6 +9,10 @@ module.exports = (token, blacklist) => {
      console.log(colors.magenta('Tibo 1.0'))
      console.log(colors.magenta('Desenvolvido por Hélio Kröger'))
      console.log(colors.magenta('https://heliojuniorkroger.com'))
+     console.log(colors.magenta('Contribuições'))
+     console.log(colors.magenta('Neto Jocelino | github.com/netojocelino'))
+     console.log(colors.magenta('Tiago Santos  | github.com/quixote15'))
+
      tiboData.headers = {
           headers: {
                'x-auth-token': token
@@ -17,13 +23,14 @@ module.exports = (token, blacklist) => {
 }
 
 const start = () => {
-     axios.get('https://api.gotinder.com/recs/core', tiboData.headers)
+     axios.get('https://api.gotinder.com/recs/core?locale=pt-BR', tiboData.headers)
           .then(res => {
+              
                res.data.results.map((person, i) => {
                     person.bio.split(' ').map(word => {
                          tiboData.blacklist.map(blacklistWord => {
                               if (word.toLowerCase() === blacklistWord) {
-                                   axios.options(`https://api.gotinder.com/pass/{person._id}`, tiboData.headers)
+                                   axios.options(`https://api.gotinder.com/pass/{person._id}?locale=pt-BR`, tiboData.headers)
                                         .then(() => {
                                              console.log(colors.red(`Recusou ${person.name}, ${new Date().getFullYear() - new Date(person.birth_date).getFullYear()} (motivo: "${blacklistWord}" na bio)`))
                                         })
@@ -34,10 +41,11 @@ const start = () => {
                               }
                          })
                     })
-                    axios.options(`https://api.gotinder.com/like/{person._id}`, tiboData.headers)
+                    axios.options(`https://api.gotinder.com/like/{person._id}?locale=pt-BR`, tiboData.headers)
                          .then(() => {
-                              console.log(colors.green(`Curtiu ${person.name}, ${new Date().getFullYear() - new Date(person.birth_date).getFullYear()}`))
-                         })
+                            getProfileData(person);
+                            console.log(colors.green(`Curtiu ${person.name}, ${new Date().getFullYear() - new Date(person.birth_date).getFullYear()}`))          
+                    })
                          .catch(err => {
                               throw err
                          })
@@ -48,3 +56,37 @@ const start = () => {
                throw err
           })
 }
+
+
+const getProfileData = (person) =>{
+    var i = 0;
+    var dir = `users/${person.name}+${person._id}/`;
+
+    //create directory if doesn't exist
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir);
+    }
+
+    for ( let data in person['photos'])
+    {      
+        // Download to a directory and save with the original filename
+        const options = {
+          url: person['photos'][i++].url,
+          dest: dir                  // Save to /path/to/dest/image.jpg
+        }
+        
+        download.image(options)
+          .then(({ filename, image }) => {
+           // console.log('File saved to', filename)
+          }).catch((err) => {
+           console.log(colors.red(`Foto de ${person.name} não foi salva.`))
+            throw err
+          })
+   }
+
+   fs.appendFile(`${dir}/bio.txt`, person.bio, (err) => { //append the person bio to the 
+    if (err) throw err;
+    });
+
+}
+
